439e2bdc59891a3a8cae3d28f60a4de7c0b48b4d
nevikw39 <nevikw39@icloud.com>
2020-04-12 00:46:23 +0800

    vec

--- 00_vec_modify_end.cpp
+++ 00_vec_modify_end.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     vector<int> v{1, 2, 3, 4, 5};
-    cout << "Now v contains " << v.size() << " elements.\n";
+    cout << "Initially v contains " << v.size() << " elements.\n";
     v.push_back(6);
     cout << "After pushing, v contains " << v.size() << " elements.\n";
     v.pop_back();
